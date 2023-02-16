1bc762d9a857cee8eabbdb4c75df834322e5c621
dwee <106995129+dwee2024@users.noreply.github.com>
2023-02-16 18:08:13 +0000

    reloads site after clicked

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// const link = document.getElementById("link");
+
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://api.publicapis.org/entries");
+// xhr.send();
+
+// xhr.onreadystatechange = function() {
+//   if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+//     const data = JSON.parse(this.responseText);
+//     let randomIndex = Math.floor(Math.random() * data.entries.length);
+//     let randomAPI = data.entries[randomIndex];
+//     link.href = randomAPI.Link;
+//     link.target = "_blank";
+//   }
+// };
 const link = document.getElementById("link");
 
 const xhr = new XMLHttpRequest();
@@ -13,3 +28,9 @@ xhr.onreadystatechange = function() {
     link.target = "_blank";
   }
 };
+
+document.getElementById("link").addEventListener("click", function(event) {
+  event.preventDefault();
+  window.open(link.href, "_blank");
+  location.reload();
+});
